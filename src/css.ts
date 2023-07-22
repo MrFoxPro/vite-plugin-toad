98b89d7e46ea1c52ebd57344dc36f6f2d9fce6fd
mrfoxpro <tuningiposadka@gmail.com>
2023-07-22 12:44:38 +0500

    chore(types): add global type

--- src/css.ts
+++ src/css.ts
@@ -1,7 +1,6 @@
 type CSS = (strings: TemplateStringsArray, ...exprs: Array<string | number>) => string
 export const css: CSS = () => void 0
-// export const createToadSelect = <T extends string>(name: T) => {
-//     return {
-//         [name]: css
-//     } as Record<T, CSS>
-// }
+
+declare global {
+    export const css: CSS 
+}
